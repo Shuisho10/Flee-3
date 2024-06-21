11f6afbf7d6a9a071a047bacf6bac291204a8f34
Shuisho10 <54242777+Shuisho10@users.noreply.github.com>
2024-06-21 17:33:38 +0200

    Comments

--- src/core/Player.gd
+++ src/core/Player.gd
@@ -3,12 +3,14 @@ extends Sprite2D
 @export var current_scene : Scroller
 
 var lane: int = 0
-@onready var anim :AnimationPlayer= %AnimationPlayer
+var locked_movement: bool = false
+var jumping: bool = false
+@onready var _anim :AnimationPlayer= %AnimationPlayer
 
 #Move player to lane
 func _process(delta):
 	position.y = lerpf(position.y,lane * current_scene.obs_size.y, delta * (current_scene.speed/20))
-	if Input.is_action_pressed("button_jump") and anim.current_animation=="":
+	if Input.is_action_pressed("button_jump") and _anim.current_animation=="":
 		jump()
 
 
@@ -21,12 +23,13 @@ func _input(_e):
 	
 
 func jump():
-	anim.play("jump",1,1/(current_scene.time_per_obsticle()*2))
+	_anim.play("jump",1,1/(current_scene.time_per_obsticle()*2))
 	
 
 func move_up():
 	if current_scene.lane_size > lane:
 		lane += 1
+	
 
 func move_down():
 	if -current_scene.lane_size < lane:
