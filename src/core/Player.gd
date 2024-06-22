b18099741f28eebdeab21519711be3c20799653e
Shuisho10 <54242777+Shuisho10@users.noreply.github.com>
2024-06-22 16:23:39 +0200

    Nearly perfect jumping

--- src/core/Player.gd
+++ src/core/Player.gd
@@ -6,14 +6,17 @@ var lane: int = 0
 var locked_movement: bool = false
 var jumping: bool = false
 var side_jump: bool = false
-@onready var _anim :AnimationPlayer= %AnimationPlayer
+@onready var _anim_player: AnimationPlayer = %AnimationPlayer
+@onready var _anim_jump: Animation = _anim_player.get_animation("jump")
 
 #Move player to lane
 func _process(delta: float):
-	position.y = lerpf(position.y,lane * current_scene.obs_size.y, delta * current_scene.speed / 20)
-	if Input.is_action_pressed("button_jump") and not is_jumping():
+	position.y = lerpf(position.y, lane * current_scene.obs_size.y, clampf(delta * current_scene.speed / 20,0,1))
+	if Input.is_action_pressed("button_jump"):
 		jump()
-	if not is_jumping() or side_jump:
+	else:
+		_anim_jump.loop_mode = Animation.LOOP_NONE
+	if not jumping or side_jump:
 		if Input.is_action_just_pressed("button_down"):
 			move_up()
 			side_jump = false
@@ -23,12 +26,13 @@ func _process(delta: float):
 
 
 func jump():
-	_anim.play("jump",-1,1/(current_scene.time_per_obsticle()*2))
-	side_jump = true
+	if not jumping:
+		if _anim_player.current_animation == "":
+			_anim_jump.loop_mode = Animation.LOOP_LINEAR
+			_anim_player.play("jump",-1,1/(current_scene.time_per_obsticle()*2))
+		side_jump = true
+		jumping=true
 	
-
-func is_jumping():
-	return _anim.current_animation == "jump"
 
 func move_up():
 	if current_scene.lane_size > lane:
@@ -38,3 +42,6 @@ func move_up():
 func move_down():
 	if -current_scene.lane_size < lane:
 		lane -= 1
+
+func end_jump():
+	jumping=false
--- src/core/Scroller.gd
+++ src/core/Scroller.gd
@@ -2,15 +2,16 @@ class_name Scroller
 extends Node2D
 signal next_obstacle
 
-var scrolled: float = 0 
+@export var chunk_pool: Array[Node2D]
 @export var speed: float = 100
+
+var scrolled: float = 0 
 var lane_size:int = 2
 var obs_count:int = 0
 var obs_size: Vector2 = Vector2(128,128)
-var chunk_spawn_distance = 650
-@export var chunk_pool: Array[Node2D]
+var _chunk_spawn_distance :int = 650
+var _wait: int = 0
 var rng: RandomNumberGenerator
-var wait: int =0
 
 func _init():
 	rng = RandomNumberGenerator.new()
@@ -22,16 +23,16 @@ func _process(delta: float):
 		obs_count = int((scrolled)/obs_size.x)
 		emit_signal("next_obstacle")
 		_spawn()
-		wait-=1
+		_wait-=1
 
 func _spawn():
-	if wait<0:
+	if _wait<0:
 		var randomint: int = rng.randi_range(0,chunk_pool.size()-1)
 		var new_chunk :Node2D = chunk_pool[randomint].duplicate()
 		new_chunk.setup(self)
 		add_child(new_chunk)
-		new_chunk.position = Vector2(chunk_spawn_distance,0)
-		wait = new_chunk.chunk_length
+		new_chunk.position = Vector2(_chunk_spawn_distance,0)
+		_wait = new_chunk.chunk_length
 	
 func time_per_obsticle():
 	return obs_size.x/speed
