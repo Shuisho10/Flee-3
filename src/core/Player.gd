c95015a121cfdd319b90c9d9bc166e92ce72a5a0
Shuisho10 <54242777+Shuisho10@users.noreply.github.com>
2024-06-22 20:31:47 +0200

    Scoring and Game over system
    - Added time based scoring
    - Added game freeze when game is over
    - Added a way to reset scene
    - Reworked jumping animation
    - Fixed invencibility

--- src/core/Player.gd
+++ src/core/Player.gd
@@ -1,28 +1,30 @@
 extends Sprite2D
+signal player_hit
 
 @export var current_scene : Scroller
+@onready var _anim_player: AnimationPlayer = %AnimationPlayer
+@onready var _anim_jump: Animation = _anim_player.get_animation("jump")
 
 var lane: int = 0
 var locked_movement: bool = false
 var jumping: bool = false
 var side_jump: bool = false
-@onready var _anim_player: AnimationPlayer = %AnimationPlayer
-@onready var _anim_jump: Animation = _anim_player.get_animation("jump")
 
-#Move player to lane
 func _process(delta: float):
-	position.y = lerpf(position.y, lane * current_scene.obs_size.y, clampf(delta * current_scene.speed / 20,0,1))
-	if Input.is_action_pressed("button_jump"):
-		jump()
-	else:
-		_anim_jump.loop_mode = Animation.LOOP_NONE
-	if not jumping or side_jump:
-		if Input.is_action_just_pressed("button_down"):
-			move_up()
-			side_jump = false
-		if Input.is_action_just_pressed("button_up"):
-			move_down()
-			side_jump = false
+	if current_scene.running:
+		position.y = lerpf(position.y, lane * current_scene.obs_size.y, clampf(delta * current_scene.speed / 20,0,1))
+		if Input.is_action_pressed("button_jump"):
+			jump()
+		else:
+			_anim_jump.loop_mode = Animation.LOOP_NONE
+		if not jumping or side_jump:
+			if Input.is_action_just_pressed("button_down"):
+				move_up()
+				side_jump = false
+			if Input.is_action_just_pressed("button_up"):
+				move_down()
+				side_jump = false
+
 
 
 func jump():
@@ -45,3 +47,18 @@ func move_down():
 
 func end_jump():
 	jumping=false
+	%Hitbox.monitoring = true
+	if %Hitbox.get_overlapping_areas():
+		game_over()
+
+func _input(_e):
+	if Input.is_action_just_pressed("button_reset"):
+		get_tree().reload_current_scene()
+
+func game_over():
+	current_scene.game_over()
+	_anim_player.stop()
+
+func _on_area_2d_area_shape_entered(_area_rid, _area, _area_shape_index, _local_shape_index):
+	if not jumping:
+		game_over()
--- src/core/Scroller.gd
+++ src/core/Scroller.gd
@@ -5,25 +5,35 @@ signal next_obstacle
 @export var chunk_pool: Array[Node2D]
 @export var speed: float = 100
 
+var running: bool = true
 var scrolled: float = 0 
 var lane_size:int = 2
 var obs_count:int = 0
+var points:int = 0
 var obs_size: Vector2 = Vector2(128,128)
 var _chunk_spawn_distance :int = 650
 var _wait: int = 0
+var _point_timer: Timer
 var rng: RandomNumberGenerator
 
 func _init():
 	rng = RandomNumberGenerator.new()
 	Engine.max_fps = 120
+	_point_timer = Timer.new()
+	add_child(_point_timer)
+	_point_timer.wait_time = 1.0
+	_point_timer.autostart = true
+	_point_timer.timeout.connect(score_by_sec)
+	
 
 func _process(delta: float):
-	scrolled += delta*speed
-	if obs_count<floor((scrolled)/obs_size.x):
-		obs_count = int((scrolled)/obs_size.x)
-		emit_signal("next_obstacle")
-		_spawn()
-		_wait-=1
+	if running:
+		scrolled += delta*speed
+		if obs_count<floor((scrolled)/obs_size.x):
+			obs_count = int((scrolled)/obs_size.x)
+			emit_signal("next_obstacle")
+			_spawn()
+			_wait-=1
 
 func _spawn():
 	if _wait<0:
@@ -33,8 +43,14 @@ func _spawn():
 		add_child(new_chunk)
 		new_chunk.position = Vector2(_chunk_spawn_distance,0)
 		_wait = new_chunk.chunk_length
-	
+
+func score_by_sec():
+	points += 1
+
 func time_per_obsticle():
 	return obs_size.x/speed
 
-#spawn at next obstacle/pattern based generation/obstacle culling
+func game_over():
+	_point_timer.stop()
+	running = false
+	speed = 0
